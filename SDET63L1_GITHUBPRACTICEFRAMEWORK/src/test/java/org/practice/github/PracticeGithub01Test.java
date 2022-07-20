3bb7e20e882849b3c8f99de5471248e6a3e08f95
sandeepPandey <pandysandeep910@gmail.com>
2022-07-20 22:38:50 +0530

    commit 1

---
+++ SDET63L1_GITHUBPRACTICEFRAMEWORK/src/test/java/org/practice/github/PracticeGithub01Test.java
@@ -0,0 +1,18 @@
+package org.practice.github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeGithub01Test {
+
+	@Test
+	public void practiceGithub01() {
+		Reporter.log("practiceGithub01", true);
+	}
+
+	@Test
+	public void practiceGithub02() {
+		Reporter.log("practiceGithub02", true);
+	}
+
+}
